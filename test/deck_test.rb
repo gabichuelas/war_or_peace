fe11ef43e9fa00c5da918b12826a3f370c85bb56
Gaby Mendez <gmendez90@gmail.com>
2020-05-05 11:28:01 -0400

    Fix typo in TestSuite name & separate some tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,7 +4,7 @@ require './lib/deck'
 require './lib/card'
 require 'pry'
 
-class CardTest < Minitest::Test
+class DeckTest < Minitest::Test
 
   def setup
     # Use setup method to create
@@ -29,17 +29,12 @@ class CardTest < Minitest::Test
   end
 
   def test_deck_w_cards
-    # card1 = Card.new(:diamond, 'Queen', 12)
-    # card2 = Card.new(:spade, '3', 3)
-    # card3 = Card.new(:heart, 'Ace', 14)
-    # cards = [card1, card2, card3]
-    #
-    # deck = Deck.new(cards)
 
     assert_equal @cards, @deck.cards
+
   end
 
-  def test_card_rank
+  def test_card_rank_from_deck
 
     assert_equal 12, @deck.rank_of_card_at(0)
     assert_equal 14, @deck.rank_of_card_at(2)
@@ -58,21 +53,23 @@ class CardTest < Minitest::Test
 
   end
 
-  def test_add_and_remove_cards
+  def test_remove_card
 
     assert_equal @card1, @deck.remove_card
     assert_equal [@card2, @card3], @deck.cards
 
     assert_equal 50.0, @deck.percent_high_ranking
 
+  end
+
+  def test_add_card
+
     card4 = Card.new(:club, '5', 5)
     @deck.add_card(card4)
 
-    assert_equal [@card2, @card3, card4], @deck.cards
+    assert_equal [@card1, @card2, @card3, card4], @deck.cards
 
-    assert_equal [@card3], @deck.high_ranking_cards
-
-    assert_equal 33.33, @deck.percent_high_ranking
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
 
   end
 
